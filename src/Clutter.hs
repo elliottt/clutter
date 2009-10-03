4b638ab186f96158fec59d60f6977cbb4a3ad725
Trevor Elliott <trevor@geekgateway.com>
2009-10-03 16:15:58 -0700

    Export the Timeline module

--- src/Clutter.hs
+++ src/Clutter.hs
@@ -1,8 +1,9 @@
 
 module Clutter ( module X ) where
 
-import Clutter.Actor as X
-import Clutter.Color as X
-import Clutter.Event as X
-import Clutter.Init  as X
-import Clutter.Stage as X
+import Clutter.Actor    as X
+import Clutter.Color    as X
+import Clutter.Event    as X
+import Clutter.Init     as X
+import Clutter.Stage    as X
+import Clutter.Timeline as X
