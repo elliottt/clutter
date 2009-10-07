72f2b390548c63a2d901223de5a9852508a41e66
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2009-10-06 23:34:47 -0700

    Just messing around

--- tests/Timelines.hs
+++ tests/Timelines.hs
@@ -8,13 +8,14 @@ main  = application $ do
   setColor stage (Color 0 0 0 0xff)
   setSize stage 320 200
 
-  rect1 <- newRectangleWithColor (Color 0x40 0x30 0xff 0x90)
+  rect1 <- newRectangleWithColor (Color 0x33 0x99 0x33 0x90)
   setSize rect1 100 100
   setPosition rect1 50 50
+  setBorderWidth rect1 3
+  setBorderColor rect1 (Color 0xff 0xff 0xff 0x90)
   stage `addActor` rect1
 
-  rect <- newRectangleWithColor (Color 0x40 0xff 0x90 0x90)
-  setSize rect 100 100
+  rect <- newTextWithColor (Font "") "Click me!" (Color 0xff 0x00 0xff 0x90)
   setPosition rect 100 100
   setReactive rect True
   stage `addActor` rect
