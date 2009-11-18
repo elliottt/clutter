03ea37f48eb0713fac875d93688448c3edb405a2
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2009-11-17 22:57:50 -0800

    Add depth behavior (and move behavior tests in a separate directory)

---
+++ src/Clutter/Behavior/Depth.hs
@@ -0,0 +1,28 @@
+module Clutter.Behavior.Depth
+  ( DepthBehavior
+  , newDepthBehavior
+  ) where
+
+import Clutter.Private
+import Clutter.GLib
+
+import Foreign.Ptr
+import Foreign.ForeignPtr
+import Foreign.C.Types
+
+newtype DepthBehavior = DepthBehavior (ForeignPtr ())
+instance ForeignObject DepthBehavior where rawPtr (DepthBehavior p) = p
+instance Behavior DepthBehavior
+
+newDepthBehavior :: Alpha -> Int -> Int -> IO DepthBehavior
+newDepthBehavior a x y = withAlpha a $ \ap ->
+  do p  <- clutter_behaviour_depth_new ap (fromIntegral x) (fromIntegral y)
+     fp <- newGObject p
+     return (DepthBehavior fp)
+
+--------------------------------------------------------------------------------
+
+foreign import ccall "clutter_behaviour_depth_new"
+  clutter_behaviour_depth_new :: Ptr () -> CInt -> CInt -> IO (Ptr ())
+
+
---
+++ tests/behavior/Depth.hs
@@ -0,0 +1,56 @@
+import Clutter
+import Clutter.Behavior.Depth
+import Clutter.Behavior
+import Clutter.Alpha
+import Clutter.AnimationMode
+import Data.IORef
+
+rectangle s =
+  do r <- newRectangleWithColor (Color 0xff 0 0 0x99)
+     setSize r 100 100
+     setPosition r 20 20
+     setBorderWidth r 2
+     setBorderColor r (Color 0xff 0xff 0xff 0x99)
+     addActor s r
+     return r
+
+flipFlop act d x y =
+  do t <- newTimeline d
+     a <- newAlpha t mLinear
+     o1 <- newDepthBehavior a y x
+     o2 <- newDepthBehavior a x y
+     r <- newIORef (Right (o1,o2))
+     apply o1 act
+     onCompleted t $
+        do v <- readIORef r
+           case v of
+             Right _    -> return ()
+             Left (a,b) ->
+               do remove a act
+                  apply b act
+                  writeIORef r (Right (b,a))
+
+     return $ do x <- readIORef r
+                 case x of
+                    Left _ -> putStrLn "no" >> return ()
+                    Right (a,b) ->
+                      do putStrLn "yes"
+                         writeIORef r (Left (a,b))
+                         start t
+       
+
+
+main = application $
+  do stage <- stageGetDefault
+     setSize stage 640 480
+     setColor stage (Clutter.Color 0 0 0 0xff)
+
+
+
+     r <- rectangle stage
+
+     flip <- flipFlop r 500 (-40) 0
+     
+     onButtonPress stage $ \_ -> flip >> return True
+     actorShow stage
+     return ()
